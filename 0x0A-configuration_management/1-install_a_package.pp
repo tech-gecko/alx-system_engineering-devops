54315870af57064baa055185acdfa140804095b8
tech-gecko <w0nderzz2001@gmail.com>
2024-06-30 08:43:06 -0700

    Update 1, task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,13 @@
-# Installs version 2.1.0 of flask from pip3 using Puppet
+# Installs version 2.1.0 of flask from pip3 using Puppet.
 
-# Ensures the python package is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Uses the puppet-python module to manage the installation of Flask
-python::pip { 'flask':
+package { 'flask':
   ensure       => '2.1.0',
   pip_provider => 'pip3',
 }
 
-# Uses the puppet-python module to manage the installation of Werkzeug
-python::pip { 'werkzeug':
+# Installs version 2.1.1 of werkzeug from pip3 using Puppet.
+
+package { 'werkzeug':
   ensure       => '2.1.1',
   pip_provider => 'pip3',
 }
